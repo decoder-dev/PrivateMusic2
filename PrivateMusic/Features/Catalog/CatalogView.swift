7b5647e25ad1dbf7b129c3cfeb9e54bd0e0d0622
decoder-dev <decoder-dev@users.noreply.github.com>
2026-07-28 22:19:18 +0300

    Fix catalog greeting getter

--- PrivateMusic/Features/Catalog/CatalogView.swift
+++ PrivateMusic/Features/Catalog/CatalogView.swift
@@ -67,7 +67,7 @@ struct CatalogView: View {
 
     private var greeting: String {
         let hour = Calendar.current.component(.hour, from: Date())
-        switch hour {
+        return switch hour {
         case 5..<12: "Доброе утро"
         case 12..<18: "Добрый день"
         case 18..<23: "Добрый вечер"
